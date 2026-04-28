5f75a9201ac24f3d622beda8b0ee1e1122c549f7
Rigock <riggit>
2026-04-28 16:12:35 -0500

    add createHashRouter

--- 07-teslo-shop/src/app.router.tsx
+++ 07-teslo-shop/src/app.router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate } from "react-router";
+import { createHashRouter, Navigate } from "react-router";
 import { ShopLayout } from "./shop/layouts/ShopLayout";
 import { HomePage } from "./shop/pages/home/HomePage";
 import { ProductPage } from "./shop/pages/product/ProductPage";
@@ -18,7 +18,8 @@ const AuthLayout = lazy(() => import('./auth/layouts/AuthLayout'));
 // eslint-disable-next-line react-refresh/only-export-components
 const AdminLayout = lazy(() => import('./admin/layouts/AdminLayout'));
 
-export const appRouter = createBrowserRouter([
+// export const appRouter = createBrowserRouter([
+export const appRouter = createHashRouter([
   // Main Routes
   {
     path: '/',
