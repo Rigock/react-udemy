f52967f5e741f5a77d3dc10867c11d5005aa480f
Rigock <riggit>
2026-04-28 16:01:27 -0500

    Feat: Update repo

---
+++ 08-teslo-shop-app/src/auth/actions/register.action.ts
@@ -0,0 +1,19 @@
+import { tesloApi } from "@/api/tesloApi";
+import type { AuthResponse } from "../interface/auth.response";
+
+
+
+export const registerAction = async(email: string, password: string, fullName: string):Promise<AuthResponse> =>{
+
+  try {
+    const { data } = await tesloApi.post<AuthResponse>('/auth/register', {
+      email,
+      password,
+      fullName,
+    });
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
